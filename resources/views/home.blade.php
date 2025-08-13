b8d40ae69f2c8809bebbe22d1fbb726058bc457f
Gulpanjul <gulpa.andhikac@gmail.com>
2025-08-13 19:56:34 +0700

    feat: make active navlink

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 <x-layout :title="$title">
       <p>Welcome to my home page</p>
-      
+
       <div class="flex mt-3">
-        @for ($i = 0; $i <= 10; $i++)
+        @for ($i = 1; $i <= 10; $i += 2)
           <div class="w-8 h-8 bg-teal-500 text-white p-0 me-1 text-xs grid place-items-center">{{ $i }}
           </div>
         @endfor
