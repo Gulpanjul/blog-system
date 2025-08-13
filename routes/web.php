96d856cda295994d9c48a633260cbf312776f24c
Gulpanjul <gulpa.andhikac@gmail.com>
2025-08-13 20:39:12 +0700

    feat: blog post page

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,21 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    $posts = [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Andhika Chandra Gulpa',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, alias temporibus est excepturi, sit sapiente nihil nulla assumenda maiores ipsa debitis suscipit voluptates, sed voluptatibus animi illo! Nulla, harum facilis.'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Andhika Chandra Gulpa',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore officiis ducimus labore, consequatur quibusdam incidunt minus eos ex pariatur fuga sit laboriosam temporibus iure tenetur aut distinctio sed perspiciatis facere.'
+        ],
+
+    ];
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/about', function () {
