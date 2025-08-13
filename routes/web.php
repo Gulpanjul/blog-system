97e6b90aff17636ac6b9657e37a1b63f7c678ed5
Gulpanjul <gulpa.andhikac@gmail.com>
2025-08-13 21:57:52 +0700

    feat: model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -8,50 +9,14 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Andhika Chandra Gulpa',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, alias temporibus est excepturi, sit sapiente nihil nulla assumenda maiores ipsa debitis suscipit voluptates, sed voluptatibus animi illo! Nulla, harum facilis.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Andhika Chandra Gulpa',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore officiis ducimus labore, consequatur quibusdam incidunt minus eos ex pariatur fuga sit laboriosam temporibus iure tenetur aut distinctio sed perspiciatis facere.'
-        ],
-
-    ];
+    $posts = Post::all();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get(
     '/posts/{slug}',
     function ($slug) {
-        $posts = [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Andhika Chandra Gulpa',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, alias temporibus est excepturi, sit sapiente nihil nulla assumenda maiores ipsa debitis suscipit voluptates, sed voluptatibus animi illo! Nulla, harum facilis.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Andhika Chandra Gulpa',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore officiis ducimus labore, consequatur quibusdam incidunt minus eos ex pariatur fuga sit laboriosam temporibus iure tenetur aut distinctio sed perspiciatis facere.'
-            ],
-        ];
-        $post = Arr::first($posts, function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
-
-        if (!$post) abort(404);
+        $post = Post::find($slug);
 
         return view('post', ['title' => 'Single Post', 'post' => $post]);
     }
